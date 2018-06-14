587bc9590918caf3936f6ac5be6f0c8f87b3005b
Alex Kibler <alexkibler@me.com>
2018-06-14 08:30:46 -0600

    basic new campaign form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import { Form, Button } from 'semantic-ui-react'
 import Layout from '../../components/Layout'
 
 class CampaignNew extends Component {
   render () {
     return (
       <Layout>
-        <h1>New Campaign</h1>
+        <h3>Create a New Campaign</h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <input type='text' />
+          </Form.Field>
+          <Button primary>Create</Button>
+        </Form>
       </Layout>
     )
   }
