c64a2f6b239d5d373b5c505ea354d73a72ca8a43
Alex Kibler <alexkibler@me.com>
2018-06-19 10:56:15 -0600

    add submit logic

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 import { Form, Input, Button, Message } from 'semantic-ui-react'
+import Campaign from '../ethereum/campaign'
+import web3 from '../ethereum/web3'
 
 class ContributeForm extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  async onSubmit (event) {
+    event.preventDefault()
+    const campaign = Campaign(this.props.address)
+
+    try {
+      const accounts = await web3.eth.getAccounts()
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether')
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   render () {
     return (
-      <Form>
+      <Form onSubmit={this.onSubmit}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
+            value={this.state.value}
+            onChange={event => this.setState({ value: event.target.value })}
             label='ether'
             labelPosition='right'
           />
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Layout from '../../components/Layout'
 import Campaign from '../../ethereum/campaign'
-import { Card } from 'semantic-ui-react'
+import { Card, Grid } from 'semantic-ui-react'
 import web3 from '../../ethereum/web3'
 import ContributeForm from '../../components/ContributeForm'
 
@@ -16,7 +16,8 @@ class CampaignShow extends Component {
       balance: summary[1],
       requestCount: summary[2],
       approverCount: summary[3],
-      manager: summary[4]
+      manager: summary[4],
+      address: props.query.address
     }
   }
 
@@ -65,8 +66,14 @@ class CampaignShow extends Component {
     return (
       <Layout>
         <h3>Campaign Details</h3>
-        {this.renderCards()}
-        <ContributeForm />
+        <Grid>
+          <Grid.Column width={10}>
+            {this.renderCards()}
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <ContributeForm address={this.props.address} />
+          </Grid.Column>
+        </Grid>
       </Layout>
     )
   }
