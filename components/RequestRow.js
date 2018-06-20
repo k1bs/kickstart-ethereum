544f2934270cbfd6d40356ef6d9b79e395aab1bf
Alex Kibler <alexkibler@me.com>
2018-06-20 15:09:00 -0600

    add ui feedback

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -36,18 +36,24 @@ class RequestRow extends Component {
 
     const { id, request, approversCount } = this.props
 
+    const readyToFinalize = request.approvalCount > (approversCount / 2)
+
     return (
-      <Row>
+      <Row disabled={request.complete} positive={readyToFinalize && !request.complete}>
         <Cell>{id}</Cell>
         <Cell>{request.description}</Cell>
         <Cell>{`${web3.utils.fromWei(request.value, 'ether')} ether`}</Cell>
         <Cell>{request.recipient}</Cell>
         <Cell>{`${request.approvalCount} / ${approversCount}`}</Cell>
         <Cell>
-          <Button color='green' basic onClick={this.onApprove}>Approve</Button>
+          {request.complete
+            ? null
+            : (<Button color='green' basic onClick={this.onApprove}>Approve</Button>)}
         </Cell>
         <Cell>
-          <Button color='teal' basic onClick={this.onFinalize}>Finalize</Button>
+          {request.complete
+            ? null
+            : <Button color='teal' basic onClick={this.onFinalize}>Finalize</Button>}
         </Cell>
       </Row>
     )
